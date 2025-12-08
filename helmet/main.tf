cb407ccb3be0350e28fd4e4085ef1bcb96fd6f49
Jane Jeon <git@janejeon.com>
2025-12-08 01:54:43 -0800

    Copy over module for sharing

---
+++ helmet/main.tf
@@ -0,0 +1,28 @@
+locals {
+  chart_rel_path = "${var.repo_root}/charts/${var.name}"
+
+  # Only hash YAML files so Helm's dependency_update doesn't change the result of fileset.
+  chart_files = fileset(local.chart_rel_path, "**/*.yaml")
+
+  chart_hash = sha256(join("", [
+    for f in local.chart_files :
+    filesha256("${local.chart_rel_path}/${f}")
+  ]))
+}
+
+resource "helm_release" "this" {
+  name      = var.name
+  namespace = var.namespace
+
+  # State stores the stable, relative string
+  chart = local.chart_rel_path
+
+  atomic            = true
+  dependency_update = true
+
+  # The magic knob: changes when any YAML file in the chart changes
+  set = [{
+    name  = "chartHash"
+    value = local.chart_hash
+  }]
+}
